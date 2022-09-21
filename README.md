6118e12be54ebbf7290ca5a2ce57c61aa4625b03
Lilly Hall <lillyhall@Lillys-MacBook-Pro.local>
2022-09-21 18:48:46 -0400

    second commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # intro_lab2_hall
 # intro_lab2_hall
 # intro_lab2_hall
+# intro_lab2_hall
---
+++ lab11final/src/module-info.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+/**
+ * @author lillyhall
+ *
+ */
+module lab11final {
+}
+
+import java.util.Scanner;
+
+public class LabProgram {
+   
+   public static int fibonacci(int n) {
+if(n<0){
+   return -1;}
+   else if(n==0){
+      return 0;}
+      else if (n==1){
+         return 1;}
+         else{
+            int firstNum=0;
+            int secondNum =1;
+            int atNum=-1;
+            for (int i=2;i<=n;i++){
+               atNum=firstNum+secondNum;
+               
+               firstNum=secondNum;
+               secondNum=atNum;}
+               return atNum;}
+}
+   
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int startNum;
+      
+      startNum = scnr.nextInt();
+      System.out.println("fibonacci(" + startNum + ") is " + fibonacci(startNum));
+   }
+}
